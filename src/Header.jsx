1124ab2307905bd42f32608be82f165de89d0c49
ryoma <ryoma1215rh@icloud.com>
2024-05-28 00:24:43 +0900

    completed base

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <container className="container">
+    <header className='header'>掲示板</header>
+    <p>スレッドを立てる</p>
+    </container>
+    </>
+  )
+}
+
+export default Header
