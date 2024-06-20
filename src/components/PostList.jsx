9065001e18554fe3edcc7f452b6c609dc12bd22a
ryoma <ryoma1215rh@icloud.com>
2024-06-20 15:07:49 +0900

    投稿フォームを追加

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+
 const PostList = () => {
     const [posts, setPosts] = useState([]);
+    const [newPost, setNewPosts] = useState('');
     const { threadId } = useParams();
     useEffect(() => {
         const getPost = async () => {
 
             try {
-                //const response = await fetch(`https://railway.bulletinboard.techtrain.dev/threads/${thread_id}/posts`);
                 const response = await fetch(`https://railway.bulletinboard.techtrain.dev/threads/${threadId}/posts`);
                 const data = await response.json();
                 // データが配列であることを確認
@@ -16,13 +17,36 @@ const PostList = () => {
                 } else {
                     console.error('Expected data to be an array:', data);
                 }
-               
+
             } catch (error) {
                 console.error('error', error);
             }
         }
         getPost();
     }, [threadId]);
+
+    const handleInputChange = (event) => {
+        setNewPosts(event.target.value);
+    }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch(`https://railway.bulletinboard.techtrain.dev/threads/${threadId}/posts`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ post: newPost }),
+            });
+            const data = await response.json();
+            setPosts([...posts, data]);
+
+        } catch (error) {
+            console.error('error', error);
+        }
+    }
+
+
     return (
         <>
             <h3 className='title'>投稿一覧</h3>
@@ -35,6 +59,17 @@ const PostList = () => {
                     ))}
                 </tbody>
             </table>
+            <form onSubmit={handleSubmit}>
+                <input className="inputfield"
+                    type='text' placeholder='投稿しよう!!'
+                    value={newPost}
+                    onChange={handleInputChange}>
+                </input>
+                <button className="button" type="submit">追加</button>
+                <Link to="/">Topに戻る</Link>
+            </form>
+
+
         </>
     )
 }
